5c2490a9d189c3d1af33c0ae500236eebb8d951d
adnan-asad01 <adnan.asad01@gmail.com>
2022-07-14 03:50:34 +0200

    a6.0

--- viz.js
+++ viz.js
@@ -4,7 +4,7 @@ var viz;
 window.onload = function() {
 
     var vizDiv = document.getElementById("myViz");
-    var url = "https://public.tableau.com/views/CovidDeathToll/DashboardCovidDeath_1?:display_count=n&:origin=viz_share_link";
+    var url = "https://public.tableau.com/views/CovidDeathToll/DashboardCovidDeath_1?:language=en-US&:display_count=n&:origin=viz_share_link";
 
     var options = {
       width: 600,
