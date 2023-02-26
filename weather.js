a007d37013a82eb5cbf77d8d2ccbf1ec4268b895
adnan-asad01 <adnan.asad01@gmail.com>
2023-02-26 17:15:10 +0100

    local time

--- weather.js
+++ weather.js
@@ -1,25 +1,37 @@
 $(document).ready(function() {
-    // Define the API key and cities to retrieve temperature data for
+    // Define the API key and cities to retrieve temperature and time data for
     var api_key = '2302790a204dcdb09dda857f85a308e0';
-    var cities = ['New York', 'London', 'Paris', 'Tokyo', 'Sydney'];
+    var cities = [
+      {name: 'New York', timezone: 'America/New_York'},
+      {name: 'London', timezone: 'Europe/London'},
+      {name: 'Paris', timezone: 'Europe/Paris'},
+      {name: 'Tokyo', timezone: 'Asia/Tokyo'},
+      {name: 'Sydney', timezone: 'Australia/Sydney'}
+    ];
   
-    // Define a function to retrieve the current temperature and weather information for a given city
+    // Define a function to retrieve the current temperature, weather information, and time for a given city
     function getWeather(city) {
-      var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api_key + '&units=metric';
+      var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city.name + '&appid=' + api_key + '&units=metric';
       $.get(url, function(data) {
-        var temp = data.main.temp;
+        var temp = Math.round(data.main.temp);
         var weatherDesc = data.weather[0].description;
         var weatherIcon = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png';
         var cityRow = $('<tr></tr>');
-        var cityCell = $('<td class="city-cell">' + city + '</td>');
-        var tempCell = $('<td class="temp-cell">' + Math.round(temp) + '°C</td>');
+        var cityCell = $('<td class="city-cell">' + city.name + '</td>');
+        var tempCell = $('<td class="temp-cell">' + temp + '°C</td>');
         var weatherCell = $('<td class="weather-cell">' + weatherDesc + '<br><img src="' + weatherIcon + '"></td>');
-        cityRow.append(cityCell, tempCell, weatherCell);
+        var timeCell = $('<td class="time-cell"></td>');
+        cityRow.append(cityCell, tempCell, weatherCell, timeCell);
         $('#temp-table').append(cityRow);
+        
+        // Get the current time in the city's timezone and display it in the time cell
+        var timezone = city.timezone;
+        var currentTime = moment.tz(moment(), timezone);
+        timeCell.text(currentTime.format('h:mm A'));
       });
     }
   
-    // Retrieve the temperature and weather data for each city and display it in a table format
+    // Retrieve the temperature, weather, and time data for each city and display it in a table format
     for (var i = 0; i < cities.length; i++) {
       getWeather(cities[i]);
     }
