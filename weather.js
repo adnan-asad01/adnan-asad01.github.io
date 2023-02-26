2235d36328c2d7c89b3ecebc59e0657dcaeb02b7
adnan-asad01 <adnan.asad01@gmail.com>
2023-02-26 17:01:00 +0100

    new style

--- weather.js
+++ weather.js
@@ -2,22 +2,26 @@ $(document).ready(function() {
     // Define the API key and cities to retrieve temperature data for
     var api_key = '2302790a204dcdb09dda857f85a308e0';
     var cities = ['New York', 'London', 'Paris', 'Tokyo', 'Sydney'];
-    
-    // Define a function to retrieve the current temperature for a given city
-    function getTemp(city) {
+  
+    // Define a function to retrieve the current temperature and weather information for a given city
+    function getWeather(city) {
       var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api_key + '&units=metric';
       $.get(url, function(data) {
         var temp = data.main.temp;
-        var cityDiv = $('<div class="col-sm-6 col-md-4 col-lg-3 mb-4"></div>');
-        var cityTitle = $('<h4 class="text-center">' + city + '</h4>');
-        var tempDisplay = $('<p class="text-center">' + temp + '°C</p>');
-        cityDiv.append(cityTitle, tempDisplay);
-        $('#temp-display').append(cityDiv);
+        var weatherDesc = data.weather[0].description;
+        var weatherIcon = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png';
+        var cityRow = $('<tr></tr>');
+        var cityCell = $('<td class="city-cell">' + city + '</td>');
+        var tempCell = $('<td class="temp-cell">' + temp + '°C</td>');
+        var weatherCell = $('<td class="weather-cell">' + weatherDesc + '<br><img src="' + weatherIcon + '"></td>');
+        cityRow.append(cityCell, tempCell, weatherCell);
+        $('#temp-table').append(cityRow);
       });
     }
-    
-    // Retrieve the temperature data for each city and display it on the page
+  
+    // Retrieve the temperature and weather data for each city and display it in a table format
     for (var i = 0; i < cities.length; i++) {
-      getTemp(cities[i]);
+      getWeather(cities[i]);
     }
   });
+  
